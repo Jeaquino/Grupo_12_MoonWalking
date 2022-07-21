ccbf6c49d5466d3a626fb2ec9b2581db78feb2a2
LulaGutierrez <lujanvicgutierrez@gmail.com>
2022-07-20 22:06:40 -0300

    termine con las rutas y puse el design en public

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.listen(port, () => console.log('Server running in http://localhost:' + port)
 //ACA HAY QUE CAMBIAR EL NOMBRE DE CADA ARCHIVO
 
 // HOME
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', '.html')))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
 // REGISTRO
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', '.html')))
 // DETALLE
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'detalle.html')))
 //LOGIN
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', '.html')))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')))
 //CARRITO
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', '.html')))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'carrito.html')))
  
