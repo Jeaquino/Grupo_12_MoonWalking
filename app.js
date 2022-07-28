1577d7669441c323a4fcbdd9260df879a156a338
Antoopizarro <antonella_pizarro@yahoo.com.ar>
2022-07-27 23:09:51 -0300

    Agregue login y registro

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ app.listen(port, () => console.log('Server running in http://localhost:' + port)
 
 //ACA HAY QUE CAMBIAR EL NOMBRE DE CADA ARCHIVO
 // REGISTRO
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')))
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')))
 // HOME
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
 
 // DETALLE
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'detalle.html')))
 //LOGIN
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')))
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')))
 //CARRITO
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'carrito.html')))
  
