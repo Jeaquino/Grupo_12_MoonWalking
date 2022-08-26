e30b0b0f86298dcbfcd3417a2c9cf718878c9cb8
Rama1433 <ramacicarelli2001@gmail.com>
2022-08-25 23:44:00 -0300

    pusheo cambios

--- Grupo_12_MoonWalking/src/controllers/productsControllers.js
+++ Grupo_12_MoonWalking/src/controllers/productsControllers.js
@@ -1,5 +1,4 @@
-const {storeProducts} = require('../data/productModule')
-const products = require('../data/productModule').loadProducts()
+const {loadProducts, storeProducts} = require('../data/productModule');
 
 
 
@@ -8,15 +7,35 @@ module.exports = {
         return res.render('./products/carrito')
     },
     edit : (req,res) => {
-        const product = products.find(product => product.id === +req.params.id)
+        const products = loadProducts();
+        const product = products.find(product => product.id === +req.params.id);
         return res.render('./products/productEdit',{
             product,
         })
     },
     update : (req,res) => {
-        return res.send(req.body)
+        const products = loadProducts();
+        const {id} = req.params;
+        const{name,brand,price,discount,size,category,} = req.body;
+        const productModify = products.map(product =>{
+            if (product.id === +id){
+                return {
+                    ...product,
+                    name : name.trim(),
+                    brand : brand.trim(),
+                    price : +price,
+                    discount : +discount,
+                    category,
+                    size : +size
+                }
+            }
+            return product
+        })
+        storeProducts(productModify);
+        return res.redirect('./products/detalle/'+ req.params.id)
     },
     detail: (req, res)=>{
+        const products = loadProducts();
          const product = products.find(product => product.id === +req.params.id);
 
         return res.render('./products/detalle',{
@@ -26,6 +45,7 @@ module.exports = {
         return res.render('./products/productAdd')
     },
     store : (req,res)=>{
+        const products = loadProducts();
         const{name,brand,price,discount} = req.body;
 
         const id = products[products.length -1].id;
@@ -45,6 +65,7 @@ module.exports = {
         return res.redirect('/')
     },
     search : (req,res) => {
+        const products = loadProducts();
         const result = products.filter(product => product.name.toLowerCase().includes(req.query.keywords.toLowerCase()))
         return res.render('./products/product',{
             products : result
